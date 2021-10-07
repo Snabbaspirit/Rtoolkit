432670d201c124957b63a253607c9d7badfe7584
Ivan Kuzmin <ivankuzmin@MacBook-Air-Ivan.local>
2021-10-07 04:39:40 +0300

    added useSelector hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./app/store";
+import ReservationCard from "./components/ReservationCard";
 
 const App: React.FC = () => {
+  const reservations = useSelector((state: RootState) => state.reservations.value)
   return (
     <div className="App">
       <div className="container">
@@ -8,6 +12,7 @@ const App: React.FC = () => {
           <div>
             <h5 className="reservation-header">Reservations</h5>
             <div className="reservation-cards-container">
+              {reservations.map((name) => <ReservationCard name={name}/>)}
             </div>
           </div>
           <div className="reservation-input-container">
